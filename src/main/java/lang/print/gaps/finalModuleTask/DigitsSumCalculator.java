9e1d713a81b25fa551a34b849c06dd2e72614429
sergei-zhirnov <s.zhirnov@protonmail.com>
2023-11-14 10:40:22 +0400

    M3, Final Task, Try 1

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,19 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int s1 = number % 10;
+        number = number / 10;
+
+        int s2 = number % 10;
+        number = number / 10;
+
+        int s3 = number % 10;
+        number = number / 10;
+
+        int s4 = number % 10;
+
+        int result = s1 + s2 + s3 + s4;
+
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        Integer objectNumber = number;
+        String stringedNumber = objectNumber.toString();
+        String result = "";
+        for (int i = 0; i < stringedNumber.length(); i++) {
+            result = stringedNumber.charAt(i) + result;
+        }
+        System.out.println(result);
     }
 }
